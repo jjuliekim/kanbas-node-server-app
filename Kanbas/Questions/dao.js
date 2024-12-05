47b3d7a300f60a34b353ff5349f625e798fc2f16
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-12-05 17:46:00 -0500

    edit questions

--- Kanbas/Questions/dao.js
+++ Kanbas/Questions/dao.js
@@ -1,6 +1,23 @@
 import model from "./model.js";
+import QuizModel from "../Quizzes/model.js";
 
 export function findQuestionsForQuiz(quizId) {
-  console.log("findQuestionsForQuiz", quizId);
   return model.find({ quiz: quizId });
+}
+
+export function createQuestion(quizId, questionData) {
+  const question = new model({ ...questionData, quiz: quizId });
+  return question.save();
+}
+
+export function updateQuestion(quizId, questionId, questionData) {
+  return model.findOneAndUpdate({ _id: questionId, quiz: quizId }, questionData, { new: true });
+}
+
+export async function deleteQuestion(quizId, questionId) {
+  // return model.findOneAndDelete({ _id: questionId, quiz: quizId });
+  await model.findOneAndDelete({ _id: questionId, quiz: quizId });
+  const quiz = await QuizModel.findById(quizId);
+  quiz.questions = quiz.questions.filter((id) => id.toString() !== questionId);
+  await quiz.save();
 }
--- Kanbas/Questions/routes.js
+++ Kanbas/Questions/routes.js
@@ -6,4 +6,24 @@ export default function QuestionRoutes(app) {
     const questions = await questionsDao.findQuestionsForQuiz(quizId);
     res.send(questions);
   });
+
+  app.post("/api/questions/:quizId", async (req, res) => {
+    const { quizId } = req.params;
+    const questionData = req.body;
+    const newQuestion = await questionsDao.createQuestion(quizId, questionData);
+    res.status(201).send(newQuestion);
+  });
+
+  app.put("/api/questions/:quizId/:questionId", async (req, res) => {
+    const { quizId, questionId } = req.params;
+    const questionData = req.body;
+    const updatedQuestion = await questionsDao.updateQuestion(quizId, questionId, questionData);
+    res.send(updatedQuestion);
+  });
+
+  app.delete("/api/questions/:quizId/:questionId", async (req, res) => {
+    const { quizId, questionId } = req.params;
+    await questionsDao.deleteQuestion(quizId, questionId);
+    res.status(204).send();
+  });
 }
